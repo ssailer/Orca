d963833e2aa4aa07574399505b8a89435d5c82ad
Simon Sailer <simon.sailer@mpi-hd.mpg.de>
2024-03-01 18:37:23 +0100

    Comment removed parameters.

--- Source/Experiments/LEGEND/L200/ORL200Model.m
+++ Source/Experiments/LEGEND/L200/ORL200Model.m
@@ -1055,13 +1055,13 @@ NSString* ORL200ModelL200FileNameChanged = @"ORL200ModelL200FileNameChanged";
             [aCmd addField: @"trigOutEnable" withBoolean:[hw trigOutEnabled:chan]];
             [aCmd addField: @"threshold"     withLong:[hw threshold:chan]];
             [aCmd addField: @"adcGain"       withLong:[hw adcGain:chan]];
-            [aCmd addField: @"trigGain"      withLong:[hw trigGain:chan]];
+//            [aCmd addField: @"trigGain"      withLong:[hw trigGain:chan]];
             [aCmd addField: @"baseLineDAC"   withLong:[hw baseline:chan]];
             [aCmd addField: @"shapeTime"     withLong:[hw shapeTime:chan]];
-            [aCmd addField: @"filterType"    withLong:[hw filterType:chan]];
-            [aCmd addField: @"flatTopTime"   withLong:[hw flatTopTime:chan]];
+//            [aCmd addField: @"filterType"    withLong:[hw filterType:chan]];
+//            [aCmd addField: @"flatTopTime"   withLong:[hw flatTopTime:chan]];
             [aCmd addField: @"poleZeroTime"  withLong:[hw poleZeroTime:chan]];
-            [aCmd addField: @"postTrigger"   withLong:[hw postTrigger:chan]];
+//            [aCmd addField: @"postTrigger"   withLong:[hw postTrigger:chan]];
             [aCmd addField: @"baselineSlew"  withLong:[hw baselineSlew:chan]];
             [aCmd addField: @"swTrigInclude" withLong:[hw swtInclude:chan]];
             
